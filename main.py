99b910962ff125bafd90eb8d38c16a6e464be49b
wagkob20 <94897208+wagkob20@users.noreply.github.com>
2022-06-13 11:31:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ intents = discord.Intents.default()
 intents.members = True
 intents.all()
 
-client = commands.Bot(command_prefix = "PREFIX", intents = intents, help_command = None,  activity = discord.Streaming(name=NAME, url=TWITCH_URL_HERE))
+client = commands.Bot(command_prefix = "f", intents = intents, help_command = None)
 #Playing -> activity = discord.Game(name="!help")
 #Streaming -> activity = discord.Streaming(name="!help", url="twitch_url_here")
 #Listening -> activity = discord.Activity(type=discord.ActivityType.listening, name="!help")
@@ -28,7 +28,9 @@ async def on_ready():
 #------------------------------------ping------------------------------------
 @client.command()
 async def ping(ctx):
-    await ctx.send(f'Pong! Latency: {round(client.latency * 1000)}ms')
+    embed = discord.Embed(title = f'Pong! Latency: {round(client.latency * 1000)}ms', color=0x1e5e31)
+    await ctx.send(embed = embed)
+
 
 #------------------------------------ban------------------------------------
 @client.command()
@@ -49,14 +51,29 @@ async def unban(ctx, *, member):
     await ctx.guild.unban(user)
     await ctx.send(f'User {member} was unbanned!') 
 
+#------------------------------------assign Role------------------------------------
+@client.command()
+@commands.has_permissions(administrator=True)
+async def assignRole(ctx, roleId : discord.Role):
+    server = client.get_guild(SERVER_ID)
+    role2 = discord.utils.get(server.roles, id = ROLE_ID) #Rolle, which gets assigned
+    print("Role assigned to:")
+    await ctx.send("done")
+    for guild in client.guilds:
+        for member in guild.members:
+            if roleId in member.roles:
+                print(member.name)
+                await member.add_roles(role2) 
+
 #------------------------------------help------------------------------------
 @client.command()
 async def help(ctx):
     embed = discord.Embed(title="HELP", description=f"Here you find a full list of all bot commands sorted by permissions:", color=0x1e5e31)
-    embed.add_field(name= "PREFIX", value= "PREFIX", inline=False)
-    embed.add_field(name= "ADMIN", value= "ban, unban", inline=False)
-    embed.add_field(name= "MODERATOR", value= "kick", inline=False)
-    embed.add_field(name= "MEMBER", value= "ping, avatar", inline=False)
+    embed.add_field(name= "PRÄFIX", value= "PREFIX", inline=False)
+    embed.add_field(name= "ADMIN", value= "ban, unban, rules, assignRole", inline=False)
+    embed.add_field(name= "MODERATOR", value= "kick, purge", inline=False)
+    embed.add_field(name= "MEMBER", value= "ping, avatar, invite", inline=False)
+    embed.set_footer(text="for any help or feedback contact OWNER_DISOCRD_TAG")
 
     await ctx.send(embed=embed)
 
@@ -74,7 +91,7 @@ async def kick(ctx, member: discord.Member, *, reason=None):
 @client.event
 async def on_member_join(member):
     server = client.get_guild(SERVER_ID)
-    channel = client.get_channel(WELCOME_CHANNEL_ID)
+    channel = client.get_channel(CHANNEL_ID)
     guild = member.guild
 
 #------------------------------------autoroles------------------------------------
@@ -86,11 +103,12 @@ async def on_member_join(member):
     await member.add_roles(role3)
 
 #------------------------------------welcome message------------------------------------
-    embed=discord.Embed(title=f"Welcome {member.name} to {member.guild.name}", description=f"If you have any questions feel free to ping/dm <@746697061222580295>", color=0x547141) 
+    embed=discord.Embed(title=f"Welcome {member.name} to {member.guild.name}", description=f"If you have any questions feel free to ping/dm <@OWNER_ID>", color=0x547141) 
     embed.set_thumbnail(url=member.avatar_url)
-    embed.set_image(url=IMAGE_URL)
+    embed.set_image(url="IMAGE_URL")
     embed.set_footer(text = f"We are now {guild.member_count} member")
 
+    await channel.send("<@&WELCOME_CHANNEL_ID>")
     await channel.send(embed=embed)
 
 #------------------------------------return prefix on ping------------------------------------
@@ -103,14 +121,45 @@ async def on_message(message):
             await message.channel.send(embed=embed)
 
     await client.process_commands(message)
+    
+#------------------------------------invite link------------------------------------
+@client.command()
+async def invite(ctx):
+    embed=discord.Embed(title="INVITE_LINK", color=0x547141)
+    embed.set_footer(text = f"requested by {ctx.author.name}")
+    await ctx.send(embed = embed)
             
 #------------------------------------purge------------------------------------
 @client.command(pass_context=True)
 @commands.has_permissions(manage_messages=True)
 async def purge(ctx, limit: int):
-    await ctx.channel.purge(limit=limit)
+    await ctx.channel.purge(limit=limit + 1)
     await ctx.send(f"{limit} messages cleared by {ctx.author.mention}")
     await ctx.message.delete()
+
+#------------------------------------output rules as embed------------------------------------
+@client.command() 
+@commands.has_permissions(administrator=True)
+async def rules(ctx):
+    embed=discord.Embed(title="Rules of「SERVER_NAME」", color=0x547141)
+    embed.add_field(name="General Rules:", value="⸺⸺⸺⸺⸺⸺", inline=False)
+    embed.add_field(name="Treat everyone with respect and be friendly when talking to others!", value="ˏ⸉ˋ‿̩͙‿̩̩̽‿̩͙ ‿̩̥̩‿̩̩̽‿̩͙ˊ⸊ˎˏ⸉ˋ‿̩͙‿̩̩̽‿̩͙ ‿̩̥̩‿̩̩̽‿̩͙ˊ⸊ˎ", inline=False)
+    embed.add_field(name="We DO NOT tolerate any form of homophobia, transphobia, racism, sexism etc. here and try to create a safe space for everyone!", value="ˏ⸉ˋ‿̩͙‿̩̩̽‿̩͙ ‿̩̥̩‿̩̩̽‿̩͙ˊ⸊ˎˏ⸉ˋ‿̩͙‿̩̩̽‿̩͙ ‿̩̥̩‿̩̩̽‿̩͙ˊ⸊ˎ", inline=False)
+    embed.add_field(name=" Do not promote any other server, website, youtube channel etc. anywhere in the server except you got permissions from an admin", value="ˏ⸉ˋ‿̩͙‿̩̩̽‿̩͙ ‿̩̥̩‿̩̩̽‿̩͙ˊ⸊ˎˏ⸉ˋ‿̩͙‿̩̩̽‿̩͙ ‿̩̥̩‿̩̩̽‿̩͙ˊ⸊ˎ", inline=False)
+    embed.add_field(name="No Alts or bot-accounts are allowed in the server!", value="ˏ⸉ˋ‿̩͙‿̩̩̽‿̩͙ ‿̩̥̩‿̩̩̽‿̩͙ˊ⸊ˎˏ⸉ˋ‿̩͙‿̩̩̽‿̩͙ ‿̩̥̩‿̩̩̽‿̩͙ˊ⸊ˎ", inline=False)
+    embed.add_field(name="Keep in mind that some bots have special rules, so make sure to inform yourself before you use them!", value="ˏ⸉ˋ‿̩͙‿̩̩̽‿̩͙ ‿̩̥̩‿̩̩̽‿̩͙ˊ⸊ˎˏ⸉ˋ‿̩͙‿̩̩̽‿̩͙ ‿̩̥̩‿̩̩̽‿̩͙ˊ⸊ˎ", inline=False)
+    embed.add_field(name="Follow the Discord-TOS [https://discord.com/terms] !", value="ˏ⸉ˋ‿̩͙‿̩̩̽‿̩͙ ‿̩̥̩‿̩̩̽‿̩͙ˊ⸊ˎˏ⸉ˋ‿̩͙‿̩̩̽‿̩͙ ‿̩̥̩‿̩̩̽‿̩͙ˊ⸊ˎ", inline=False)
+    embed.add_field(name="Voice Rules: ", value="⸺⸺⸺⸺⸺⸺", inline=False)
+    embed.add_field(name="Do not abuse your mic!", value="ˏ⸉ˋ‿̩͙‿̩̩̽‿̩͙ ‿̩̥̩‿̩̩̽‿̩͙ˊ⸊ˎˏ⸉ˋ‿̩͙‿̩̩̽‿̩͙ ‿̩̥̩‿̩̩̽‿̩͙ˊ⸊ˎ", inline=False)
+    embed.add_field(name="Avoid streaming / showing NSFW or offensive content in any ways!", value="ˏ⸉ˋ‿̩͙‿̩̩̽‿̩͙ ‿̩̥̩‿̩̩̽‿̩͙ˊ⸊ˎˏ⸉ˋ‿̩͙‿̩̩̽‿̩͙ ‿̩̥̩‿̩̩̽‿̩͙ˊ⸊ˎ", inline=False)
+    embed.add_field(name="Admins are allowed to join any voice chats at any time to moderate them!", value="ˏ⸉ˋ‿̩͙‿̩̩̽‿̩͙ ‿̩̥̩‿̩̩̽‿̩͙ˊ⸊ˎˏ⸉ˋ‿̩͙‿̩̩̽‿̩͙ ‿̩̥̩‿̩̩̽‿̩͙ˊ⸊ˎ", inline=False)
+    embed.add_field(name="Text Rules:", value="⸺⸺⸺⸺⸺⸺", inline=False)
+    embed.add_field(name="Use bot commands only in the corresponding channels", value="ˏ⸉ˋ‿̩͙‿̩̩̽‿̩͙ ‿̩̥̩‿̩̩̽‿̩͙ˊ⸊ˎˏ⸉ˋ‿̩͙‿̩̩̽‿̩͙ ‿̩̥̩‿̩̩̽‿̩͙ˊ⸊ˎ", inline=False)
+    embed.add_field(name="Ecchi content is allowed to an extend! NSFW only in NSFW channels", value="ˏ⸉ˋ‿̩͙‿̩̩̽‿̩͙ ‿̩̥̩‿̩̩̽‿̩͙ˊ⸊ˎˏ⸉ˋ‿̩͙‿̩̩̽‿̩͙ ‿̩̥̩‿̩̩̽‿̩͙ˊ⸊ˎ", inline=False)
+    embed.add_field(name="Only use english, since this is the only language we can moderate!", value="ˏ⸉ˋ‿̩͙‿̩̩̽‿̩͙ ‿̩̥̩‿̩̩̽‿̩͙ˊ⸊ˎˏ⸉ˋ‿̩͙‿̩̩̽‿̩͙ ‿̩̥̩‿̩̩̽‿̩͙ˊ⸊ˎ", inline=False)
+    embed.add_field(name="Avoid sensitive topics like politics", value="ˏ⸉ˋ‿̩͙‿̩̩̽‿̩͙ ‿̩̥̩‿̩̩̽‿̩͙ˊ⸊ˎˏ⸉ˋ‿̩͙‿̩̩̽‿̩͙ ‿̩̥̩‿̩̩̽‿̩͙ˊ⸊ˎ", inline=False)
+    embed.add_field(name="Breaking the rules can result in mute, kick or ban!", value="For any questions ping <@746697061222580295>", inline=False)
+    await ctx.send(embed=embed)  
 
 #------------------------------------avatar------------------------------------
 @client.command()
@@ -136,4 +185,4 @@ async def on_command_error(ctx, error):
         print(error)
 
 #------------------------------------token------------------------------------       
-client.run(TOKEN)
+client.run("TOKEN")
